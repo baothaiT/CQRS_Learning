f2b1b4e51e3301b7639c2aaa967c7ca329e93320
DESKTOP-D1B32EL\bao <thaibao0225@gmail.com>
2024-09-28 15:31:44 +0700

    Update browser DTO

--- src/CQRS.Contract/Share/DTO/Browser/BrowserBaseDto.cs
+++ src/CQRS.Contract/Share/DTO/Browser/BrowserBaseDto.cs
@@ -11,5 +11,13 @@ public class BrowserBaseDto
     public string Name { get; set; }
     public string Path { get; set; }
     public bool IsStatus { get; set; }
+
+    public decimal? XPosition { get; set; }
+    public decimal? YPosition { get; set; }
+    public decimal? WithScreeen { get; set; }
+    public decimal? HightScreen { get; set; }
+    public decimal? Scale { get; set; }
+    public string? UserAgent { get; set; }
+
     public DateTime CreateDate { get; set; }
 }
